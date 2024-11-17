1f6e69a3a9f0cf437a75a04073ea793226668a65
akintolatechnologies <akintolatechnologies@gmail.com>
2024-11-17 19:45:45 +0100

    Frontend Cleanup

--- webcms/context_processors.py
+++ webcms/context_processors.py
@@ -1,10 +1,12 @@
 # webcms/context_processors.py
-from .models import WebDetails
+from .models import WebDetails, LandingContent
 
 
 def web_details(request):
     # Fetch the first WebDetails object (you can customize the query if necessary)
     details = WebDetails.objects.first()
+    landing = LandingContent.objects.all()
     return {
-        'business': details  # Passing the whole object to access all fields in the template
+        'business': details,
+        'landing': landing
     }
